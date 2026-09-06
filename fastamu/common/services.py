a523c8af2ab34812ebaa4465cbcfed7a1fac0a93
stupidprogrammer4 <captainjoker757@gmail.com>
2026-09-06 15:13:52 +0330

    refactor(services): move shared services out of bases

--- fastamu/common/services.py
+++ fastamu/common/services.py
@@ -1,11 +1,11 @@
 from typing import Any, Callable, Sequence, TypeVar, get_args, get_origin
 
-from fastamu.common.bases.models import BaseIDModel, BaseModel
-from fastamu.common.bases.results import BatchResultType
 from fastamu.common.errors.exceptions import (
     NotFoundException,
     ValidationException,
 )
+from fastamu.common.models.base import BaseIDModel, BaseModel
+from fastamu.common.schemas.results import BatchResultType
 from fastamu.core import resources
 
 
